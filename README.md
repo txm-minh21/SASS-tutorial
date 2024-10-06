14baf1625689a0acc06a0d23e7792cfda31dd307
TxmMinh <txmminh21@gmail.com>
2024-10-06 23:38:10 +0700

    create: variable-partials-@import

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-- create package.json file:
+# create package.json file:
     npm init
 
-- install Gulp
-npm install gulp gulp-sass sass --save-dev
+# install Gulp
+    npm install gulp gulp-sass sass --save-dev
 
-To compile scss in terminal run:
-gulp
+# To compile scss in terminal run:
+    gulp
--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('index.scss')
-        .pipe(sass())
-        .pipe(dest('css'))
+    return src('sass/**/*.scss') // Target all SCSS files in the 'sass' folder and subfolders
+        .pipe(sass()) // Compile SCSS to CSS
+        .pipe(dest('css')) // Output compiled CSS to the 'css' folder
 }
 
-
 function watchTask() {
-    watch(['index.scss'], buildStyles)
+    watch(['sass/**/*.scss'], buildStyles) // Watch all SCSS files in 'sass' folder
 }
 
 exports.default = series(buildStyles, watchTask)
