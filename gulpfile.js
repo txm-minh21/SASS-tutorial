898d105e25932507660ad3172dcac7059d05e35e
TxmMinh <txmminh21@gmail.com>
2024-10-08 16:37:12 +0700

    create: finish

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-    return src('sass/**/*.scss') // Target all SCSS files in the 'sass' folder and subfolders
+    return src('sass/**/*.scss', 'library/**/*.scss') // Target both 'sass' and 'library' folders and subfolders
         .pipe(sass()) // Compile SCSS to CSS
-        .pipe(purgecss({ content: ['*.html'] }))
+        .pipe(purgecss({ content: ['*.html'] })) //  remove unused CSS from the generated styles.
         .pipe(dest('css')) // Output compiled CSS to the 'css' folder
 }
 
 function watchTask() {
-    watch(['sass/**/*.scss', '*.html'], buildStyles) // Watch all SCSS files in 'sass' folder
+    watch(['sass/**/*.scss', 'library/**/*.scss', '*.html'], buildStyles) // Watch all SCSS files in 'sass' folder
 }
 
 exports.default = series(buildStyles, watchTask)
